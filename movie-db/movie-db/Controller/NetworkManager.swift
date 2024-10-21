fcf6c5234ba54213cf8b8b3c867fd1c3729683a8
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2024-10-21 11:40:02 +0500

    implemented NetworkManaer
    loadMovie, loadImage funcs

--- movie-db/movie-db/Controller/NetworkManager.swift
+++ movie-db/movie-db/Controller/NetworkManager.swift
@@ -6,32 +6,62 @@
 //
 
 import Foundation
+import UIKit
 
 class NetworkManager {
     
     static var shared = NetworkManager()
     
     private let apiKey = "caf3a6290939575c31752adbb8bd341d"
-    private let urlString = "https://api.themoviedb.org"
     private let session = URLSession(configuration: .default)
-    
+    private let urlImageString = "https://image.tmdb.org/t/p/w500"
     private lazy var urlComponents: URLComponents = {
         var urlComp = URLComponents()
         urlComp.scheme = "https"
         urlComp.host = "api.themoviedb.org"
         urlComp.queryItems = [
-        URLQueryItem(name: "api_key", value: apiKey)]
-        
+            URLQueryItem(name: "api_key", value: apiKey)
+        ]
         return urlComp
     }()
     
-    func loadMovie() {
+    func loadMovie(complition: @escaping (Movie)->()) {
         urlComponents.path = "/3/movie/now_playing"
         guard let url = urlComponents.url else { return }
-        let task = session.dataTask(with: url) { data, _, error in
-            if let error {
-                print(error)
+        DispatchQueue.global().async {
+            let task = self.session.dataTask(with: url) { data, _, error in
+                if let error {
+                    print(error)
+                }
+                guard let data else { return }
+                if let movies = try? JSONDecoder().decode(Movie.self,from: data) {
+                    DispatchQueue.main.async {
+                        complition(movies)
+                    }
+                }
             }
+            task.resume()
+        }
+    }
+    
+    func loadImage(posterPath: String, complition: @escaping (UIImage) -> ()) {
+        let urlString = urlImageString + posterPath
+        guard let url = URL(string: urlString) else { return }
+        DispatchQueue.global().async {
+            let task = self.session.dataTask(with: url) { data, _, error in
+                if let error {
+                    print(error)
+                }
+                guard let data else { return }
+                DispatchQueue.main.async {
+                    if let image = UIImage(data: data) {
+                        complition(image)
+                    }
+                }
+            }
+            task.resume()
         }
     }
 }
+
+
